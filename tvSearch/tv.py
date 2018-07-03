f2a66571fe5f9bff57b67e997bfe9d9e9dbfdf7e
vgutta <vineethg@udel.edu>
2018-07-03 10:58:17 -0400

    scrapes and write to csv

--- tvSearch/tv.py
+++ tvSearch/tv.py
@@ -10,11 +10,36 @@ raw_page.close()
 page = soup(html, 'html.parser')
 item_list = page.findAll("a", {"class": "itemTitle"})
 
-item_prices = page.findAll("div", {"class": "itemPrice"})
+item_info = page.findAll("div", {"class": "priceLine"})
 
+titles = []
+prices = []
+
+filename = "televisions.csv"
+f = open(filename, "w")
+headers = "Product, Price\n"
+f.write(headers)
+
+for item in item_info:
+    itemTitle = item["title"]
+    titles.append(itemTitle)
+    price = item.find("div", {"class": "itemPrice"})
+    price = price.text
+    price = price.strip()
+    prices.append(price)
+    f.write(itemTitle.replace(",", ";") + "," + price + "\n")
+
+f.close()
+
+print(len(prices), len(titles))
+print()
+
+item_prices = page.findAll("div", {"class": "itemPrice"})
+"""
 for price in page.findAll("div", "itemPrice"):
-    print(price.text)
+    println(price.text)
 
 for item in item_list:
     print(item.text)
-    """print(item_prices.text)"""
+    print(item_prices.text)
+"""
